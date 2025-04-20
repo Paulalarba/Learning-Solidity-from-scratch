84e8642037f567222106817c1ad37bc9807e9029
Paul Alarba <143729963+Paulalarba@users.noreply.github.com>
2025-04-20 15:15:12 +0800

    Create basicDataType.sol

---
+++ basicDataType.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract myContract{
+    uint256 age = 19; //only positive numbers
+    int num = -10; //positive and negative number
+    bool gwapoAko = true; //True or false logic
+    string name = "PAUL"; // stored text note string used more gas than numbers;
+    address myAddress = 0xa7b1A73848c0574afa133c00df28e0A0f0507B68; // Used to store an Ethereum wallet or contract address
+    bytes32 favNumber = "69";
+}
