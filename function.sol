f4918555fa5b97d3f6688b0910be159f994faac2
Paul Alarba <143729963+Paulalarba@users.noreply.github.com>
2025-04-22 11:46:35 +0800

    Create function.sol

---
+++ function.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+//basic function syntacs
+/*function functionName(type parameter) visibility returns (type) {
+    // logic here
+}*/
+
+contract firstContract {
+    uint256 public number;
+
+    //diri pwedi maka change value sang number
+    function setNumber(uint256 _number) public{
+        number = _number;
+    }
+    //kay view man ang naka butang meaning mo tanaw ra sya sang value
+    function getNumber() public view returns (uint256){
+        return number;
+    }
+
+}
