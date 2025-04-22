9570cecaa300c5276e35daf3118779d566ebc890
Paul Alarba <143729963+Paulalarba@users.noreply.github.com>
2025-04-22 12:00:01 +0800

    Update function.sol

--- function.sol
+++ function.sol
@@ -7,6 +7,7 @@ pragma solidity ^0.8.0;
 }*/
 
 contract firstContract {
+    //if dili butangan sang visibility ang default kay internal
     uint256 public number;
 
     //diri pwedi maka change value sang number
