974de36156eefdf5450ba428e48ab03675e9a0fb
PAUL ALARBA <alarbapaulax@gmail.com>
2025-09-23 03:45:50 +0800

    basics

--- datatype.sol
+++ datatype.sol
@@ -2,3 +2,16 @@
 pragma solidity ^0.8.0;
 
 // Local decleares inside the function and not stored in chain
+contract helloword{
+ // three type of variable local, state, global
+ // local declared inside a fuction and not store on chain
+ // state declared outside the funtion to maintain global state within th coract
+ // state is stored on chain
+//  // global  Provide information about the chain. They are injected by the EVM during runtime and are not declared by the contract author.
+
+// Include things like transaction sender, block hash, timestamp, etc.
+
+// Examples of global variables
+
+
+}
